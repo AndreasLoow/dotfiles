81ba282b25c83f5813d4c960336ccf5f835d2d7c
Andreas Lööw <AndreasLoow@users.noreply.github.com>
2016-11-10 09:34:31 +0100

    Disable electric indent mode for SML.

--- emacs/my-sml.el
+++ emacs/my-sml.el
@@ -1,6 +1,8 @@
 (ensure-packages '(sml-mode))
 
-(defun my-sml-mode-hook () "Local defaults for SML mode"
-       (setq indent-tabs-mode nil))
+(defun my-sml-mode-hook ()
+  (progn
+    (setq indent-tabs-mode nil)
+    (electric-indent-mode -1)))
 
 (add-hook 'sml-mode-hook 'my-sml-mode-hook)
