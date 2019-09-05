3197c1ff1c07c067446c80c543bcae013db7177a
Andreas Lööw <AndreasLoow@users.noreply.github.com>
2019-09-05 17:45:28 +0200

    Some Haskell things

--- emacs/my-haskell.el
+++ emacs/my-haskell.el
@@ -1,2 +1,14 @@
 (ensure-packages '(haskell-mode))
 
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(custom-set-variables
+ ;'(haskell-process-type 'cabal-repl)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t))
+
+;(define-key interactive-haskell-mode-map (kbd "M-.") 'haskell-mode-goto-loc)
+;(define-key interactive-haskell-mode-map (kbd "C-c C-t") 'haskell-mode-show-type-at)
